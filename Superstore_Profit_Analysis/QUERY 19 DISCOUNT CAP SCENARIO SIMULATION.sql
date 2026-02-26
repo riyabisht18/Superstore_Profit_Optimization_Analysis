7d8b47e98f55b8827dd9410933ff687b97f2df12
Riya Bisht <bishtriya670@gmail.com>
2026-02-26 15:20:51 +0530

    Update QUERY 19 DISCOUNT CAP SCENARIO SIMULATION.sql

--- Superstore_Profit_Analysis/QUERY 19 DISCOUNT CAP SCENARIO SIMULATION.sql
+++ Superstore_Profit_Analysis/QUERY 19 DISCOUNT CAP SCENARIO SIMULATION.sql
@@ -2,7 +2,7 @@
 -- ============================================
 -- QUERY 19: DISCOUNT CAP SCENARIO SIMULATION
 -- PURPOSE: Model profit impact of capping discounts at 20%
--- FINDING: 20% cap increases profit 62% to $463,782, achieves 20.19% margin
+-- FINDING: 20% cap increases profit 62% to $463,782, achieves 20.2% margin
 -- ============================================
 
 
@@ -15,3 +15,4 @@ SELECT
     ROUND((SUM(CASE WHEN discount > 0.2 THEN sales * 0.1158 ELSE profit END) 
            - SUM(profit)) / SUM(profit) * 100, 2) AS profit_improvement_pct
 FROM Superstore_data;
+
